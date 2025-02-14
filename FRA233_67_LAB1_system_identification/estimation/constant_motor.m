9c04da9bcc3e88759d56dda4d56dc1956daec05f
lyDevper <luckydekdee@gmail.com>
2025-02-14 19:50:01 +0700

    predicted with new estimated model K

---
+++ FRA233_67_LAB1_system_identification/estimation/constant_motor.m
@@ -0,0 +1,75 @@
+sin0d1%%
+motor_B = 2.6323e-05
+motor_Eff = 0.95727
+motor_J = 3.7051e-05
+motor_Ke = 0.056363
+sin1d0%%
+motor_B = 0.00020734
+motor_Eff = 0.94885
+motor_J = 3.109e-05
+motor_Ke = 0.043346
+sin2d0%%
+motor_B = 0.00020978
+motor_Eff = 0.96994
+motor_J = 2.3723e-05
+motor_Ke = 0.041547
+ramp1d2%%
+motor_B = 7.3613e-05
+motor_Eff = 0.61357
+motor_J = 0.00079154
+motor_Ke = 0.03344
+ramp2d4%%
+motor_B = 6.7499e-05
+motor_Eff = 0.75916
+motor_J = 0.00042058
+motor_Ke = 0.050461
+ramp3d6%%
+motor_B = 6.0098e-05
+motor_Eff = 0.81158
+motor_J = 0.00032054
+motor_Ke = 0.051696
+pulse0d1%%
+motor_B = 4.6129e-05
+motor_Eff = 0.99639
+motor_J = 9.5061e-05
+motor_Ke = 0.05402
+pulse1d0%%
+motor_B = 5.3684e-05
+motor_Eff = 0.92825
+motor_J = 4.978e-05
+motor_Ke = 0.053913
+pulse2d0%%
+motor_B = 1.7613e-06
+motor_Eff = 0.97156
+motor_J = 4.1894e-05
+motor_Ke = 0.058819
+stair1%%
+motor_B = 3.9282e-05
+motor_Eff = 0.97111
+motor_J = 2.1951e-05
+motor_Ke = 0.057961
+stair2%%
+motor_B = 5.1174e-07
+motor_Eff = 0.99684
+motor_J = 8.4638e-06
+motor_Ke = 0.060709
+stair3%%
+motor_B = 3.9633e-05
+motor_Eff = 0.99796
+motor_J = 4.2896e-05
+motor_Ke = 0.056713
+chirp1%%
+motor_B = 0.00019881
+motor_Eff = 0.90645
+motor_J = 3.1761e-05
+motor_Ke = 0.048118
+chirp2%%
+motor_B = 3.9083e-05
+motor_Eff = 0.93403
+motor_J = 3.1699e-05
+motor_Ke = 0.053174
+chirp3
+motor_B = 0.00019808
+motor_Eff = 0.93165
+motor_J = 2.8952e-05
+motor_Ke = 0.044514
